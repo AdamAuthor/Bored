62eac2373e13fa140920b74a84f47ddbe39d742c
adamauthor <ahmediarolzasov@gmail.com>
2022-07-13 08:15:41 +0600

    BORED_CROPPED

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import Navbar from "./Navbar";
 import Paper from "./Paper";
-import Buttons from "./Buttons"
 function App() {
     return (
         <>
             <Navbar/>
             <Paper/>
-            <Buttons/>
         </>
     )
 }
